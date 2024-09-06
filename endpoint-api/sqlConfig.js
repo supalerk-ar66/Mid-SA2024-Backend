e92d29407893da0bd64603a8d6686b5fff9332fa
supalerk-ar66 <supalerk_ar66@live.rmutl.ac.th>
2024-09-06 14:28:04 +0700

    Edit: File SQL config

--- endpoint-api/sqlConfig.js
+++ endpoint-api/sqlConfig.js
@@ -1,9 +1,9 @@
 var dbconfig = {
-    web_labDB: {
-        server: '192.168.56.106',
-        database:'team0_web_labDB',
+    development: {
+        server: '10.120.194.90',
+        database:'team1_web_labDB',
         user:'sa',
-        password:'',
+        password:'P@ssw0rd',
         port: 1433,
         options:{
             encript: true,
@@ -11,7 +11,22 @@ var dbconfig = {
             enableArithAbort: true,
             trustServerCertificate: true,
             trustedconnection:  true,
-            instancename:  '192.168.56.106'  // SQL Server instance name
+            instancename:  '10.120.194.90'  // SQL Server instance name
+        }
+    },
+    production: {
+        server: '10.21.43.203',
+        database:'team1_web_labDB',
+        user:'sa',
+        password:'P@ssw0rd',
+        port: 1433,
+        options:{
+            encript: true,
+            setTimeout: 12000,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+            trustedconnection:  true,
+            instancename:  '10.21.43.203'  // SQL Server instance name
         }
     },
 
